26e1dff721fbff6452d0a3afb8c7922b6ae0df2b
samuel <samuel2550k@gmail.com>
2024-04-16 19:56:10 -0500

    subo cambios de ope-aritmeticos

---
+++ ope-aritmeticos.js
@@ -0,0 +1,30 @@
+// Suma
+let resultadoSuma = 10 + 5;
+console.log(`la suma es: ${resultadoSuma}`); //15
+
+//Resta 
+let resultadoResta =20 - 7;
+console.log (`la resta es : ${resultadoResta}`); //13
+
+//Multplicacion 
+let resultadoMultiplicacion = 3 * 4;
+console.log (`la multiplicacion es ${resultadoMultiplicacion}`); //12
+
+// División
+let resultadoDivision = 16 / 4;
+console.log(`la divicion es: ${resultadoDivision}`); //4
+
+// Residuo 
+let resultadoResiduo = 10 % 3;
+console-log(`el residuo es ${resultadoResiduo}`); //1
+
+// Incremento
+let numero = 1;
+numero ++;
+console.log (`el numero es ${numero}`) //2
+
+// Decremento
+let otroNumero = 5;
+otroNumero--;
+console.log (`el otro numero es ${otroNumero}`); //4
+
